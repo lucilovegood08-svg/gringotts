e371d6c830b4169cfb8028e3b5f06a2b3ab798e7
lucilovegood08-svg <lucilovegood08@gmail.com>
2025-12-29 09:03:09 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -80,4 +80,6 @@ app.post('/delete-user',(req,res)=>{
   res.json({message:'Usuario borrado'});
 });
 
-app.listen(3000,()=>console.log('Servidor corriendo en puerto 3000'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
